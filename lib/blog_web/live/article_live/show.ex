a5bb87e624336d749cdc5a3764cb90ef2b6710fa
Thiago Ramos <thiagoramos.al@gmail.com>
2022-11-05 13:11:48 -0300

    Added minutes to read an article

--- lib/blog_web/live/article_live/show.ex
+++ lib/blog_web/live/article_live/show.ex
@@ -5,18 +5,29 @@ defmodule BlogWeb.ArticleLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:about_active, false)}
+    {:ok,
+     socket
+     |> assign(:about_active, false)
+     |> assign(:total_read_time, 0)}
   end
 
   @impl true
   def handle_params(%{"slug" => slug}, _, socket) do
+    article = get_article_by_slug(slug)
+
     {:noreply,
      socket
-     |> assign(:article, get_article_by_slug(slug))}
+     |> assign(:article, article)
+     |> assign(:total_read_time, get_total_read_time(article))}
   end
 
   defp get_article_by_slug(slug) do
     article = Posts.get_article_by_slug(slug)
     %{article | html_body: Highlighter.highlight(article.html_body)}
+  end
+
+  defp get_total_read_time(%{body: body}) do
+    total_words = body |> String.split(" ") |> Enum.count()
+    div(total_words, 250)
   end
 end
