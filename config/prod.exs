97053fbcc317d456b9c4dd6dd7b530558079243e
Thiago Ramos <thiagoramos.al@gmail.com>
2026-05-09 20:37:01 -0300

    Changed postgres version and set /up outside of https

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,10 @@ import Config
 # before starting your production server.
 config :blog, BlogWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [
+    rewrite_on: [:x_forwarded_proto],
+    exclude: [hosts: ["localhost", "127.0.0.1"], paths: ["/up"]]
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
